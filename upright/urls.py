751682781337e0a12f695ffaac57a7b349871cac
SerJ Zyabkin <s.zyabkin@ians.aero>
2021-05-17 19:48:20 +0300

    Продукция одна категория

--- upright/urls.py
+++ upright/urls.py
@@ -1,4 +1,4 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [path('', views.index, name='index')]
+urlpatterns = [path('', views.index, name='index'), path('products/', views.products, name='products')]
